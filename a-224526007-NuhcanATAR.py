9f62c960bb9b59a808a554793d4dcc4a1621b848
Nuhcan <nuhcanatar20@gmail.com>
2023-11-10 18:36:18 +0300

    a-224526007-NuhcanATAR.py

--- a-224526007-NuhcanATAR.py
+++ a-224526007-NuhcanATAR.py
@@ -3,95 +3,139 @@ from tkinter import ttk
 from bs4 import BeautifulSoup
 import requests
 
-def get_exchange_rates():
-    urlDolar = "https://www.google.com/finance/quote/USD-TRY?hl=tr"
-    dolarPage = requests.get(urlDolar)
-    htmlDOLARPAGE = BeautifulSoup(dolarPage.content, "html.parser")
-    dolarValue = float(htmlDOLARPAGE.find("div", class_="YMlKec fxKbKc").getText().replace(",", "."))
+dolarUrl = "https://www.google.com/finance/quote/USD-TRY?hl=tr"
+requestURLDolar = requests.get(dolarUrl)
 
-    urlEuro = "https://www.google.com/finance/quote/EUR-TRY?hl=tr"
-    euroPage = requests.get(urlEuro)
-    htmlEUROPAGE = BeautifulSoup(euroPage.content, "html.parser")
-    euroValue = float(htmlEUROPAGE.find("div", class_="YMlKec fxKbKc").getText().replace(",", "."))
+euroUrl = "https://www.google.com/finance/quote/EUR-TRY?hl=tr"
+requestURLEuro = requests.get(euroUrl)
 
-    return dolarValue, euroValue
-
-def temizle():
-    e_miktar_giris.delete(0, END)
-    lbl_sonuc.config(text="")
+def getExchangeData():
+    webDolarPage = BeautifulSoup(requestURLDolar.content, "html.parser")
+    valueDolar = float(webDolarPage.find("div", class_="YMlKec fxKbKc").getText().replace(",","."))
     
-def hesapla():
+    webEuroPage = BeautifulSoup(requestURLEuro.content, "html.parser")
+    valueEuro = float(webEuroPage.find("div", class_="YMlKec fxKbKc").getText().replace(",","."))
+    
+    return valueDolar, valueEuro
+    
+def calculateExchange():
     try:
-        miktar = float(e_miktar_giris.get())
-        girilen_kur = cmb_girilenkur.get()
-        cevirilecek_kur = cmb_cevirilecekkur.get()
-
-        if girilen_kur == "TL":
-            girilen_kur_orani = 1
-        elif girilen_kur == "USD":
-            girilen_kur_orani = dolarValue
-        elif girilen_kur == "EURO":
-            girilen_kur_orani = euroValue
+        inputValue = float(input_widget.get())
+        exchangeValue1 = cmbx_ex1_widget.get()
+        exchangeValue2 = cmbx_ex2_widget.get()
+        
+        if exchangeValue1 == "USD":
+            exchange = valueDolar
+        elif exchangeValue1 == "EURO":
+            exchange = valueEuro
+        elif exchangeValue1 == "TL":
+            exchange = 1
         else:
-            girilen_kur_orani = 0
-        if cevirilecek_kur == "TL":
-            cevirilecek_kur_orani = 1
-        elif cevirilecek_kur == "USD":
-            cevirilecek_kur_orani = dolarValue
-        elif cevirilecek_kur == "EURO":
-            cevirilecek_kur_orani = euroValue
+            exchange = 0
+        
+        
+        if exchangeValue2 == "USD":
+            exchange2 = valueDolar
+        elif exchangeValue2 == "EURO":
+            exchange2 = valueEuro
+        elif exchangeValue2 == "TL":
+            exchange2 = 1
         else:
-            cevirilecek_kur_orani = 0
-        sonuc = (miktar * girilen_kur_orani) / cevirilecek_kur_orani
-        lbl_sonuc.config(text=f"Girilen : {miktar} {girilen_kur} = {sonuc:.2f} {cevirilecek_kur}")
+            exchange2 = 0
+            
+        value = (inputValue * exchange) / exchange2
+            
+        labelValue_widget.config(text=f"{inputValue} {exchangeValue1} = {value:.2f} {exchangeValue2}")
     except ValueError:
-        lbl_sonuc.config(text="Geçersiz miktar!")
-
-    update_exchange_rates()
-
-def update_exchange_rates():
-    global dolarValue, euroValue
-    dolarValue, euroValue = get_exchange_rates()
-    lbl_hesapsonuc.config(text="Değerler Yazdırıldı!.")
-    anaform.after(2000, lambda: lbl_hesapsonuc.config(text=""))
+        labelValue_widget.config(text="Geçersiz Değer!!")
+    updateExchange()
     
 
-anaform = Tk()
-anaform.title("Kur Hesapla Pyhton")
-anaform.geometry("400x300")
+def updateExchange():
+    global valueDolar, valueEuro
+    valueDolar, valueEuro = getExchangeData()
+    
+def clearAll():
+    labelValue_widget.config(text="....")
+    input_widget.delete(0, END)
+    
+mainView = Tk()
 
-lbl_miktar = Label(anaform, text="Miktar:")
-lbl_miktar.grid(row=0, column=0,pady=20)
+mainView.title("Kur Hesapla")
+mainView.geometry("400x400")
 
-lbl_girilenkur = ttk.Label(anaform, text="Girilen Kur:")
-lbl_girilenkur.grid(row=1, column=0, pady=5)
+label1_widget = Label(
+    mainView,
+    fg="black",
+    font="Nunito 10 bold"
+   )
+label1_widget.grid(row=0,column=1,padx=10,pady=10)
 
-lbl_cevirilecekkur = ttk.Label(anaform, text="Çevrilecek Kur:")
-lbl_cevirilecekkur.grid(row=2, column=0, pady=5)
-
-lbl_sonuc = ttk.Label(anaform, text="")
-lbl_sonuc.grid(row=4, column=1, columnspan=2)
-
-cmb_girilenkur = ttk.Combobox(anaform, values=["TL", "USD", "EURO"])
-cmb_girilenkur.grid(row=1, column=1, padx=20, pady=5)
-
-e_miktar_giris = ttk.Entry(anaform)
-e_miktar_giris.grid(row=0, column=1, padx=20)
-
-cmb_cevirilecekkur = ttk.Combobox(anaform, values=["TL", "USD", "EURO"])
-cmb_cevirilecekkur.grid(row=2, column=1, padx=20, pady=5)
-
-btn_hesapla_guncelle = ttk.Button(anaform, text="Hesapla", command=hesapla)
-btn_hesapla_guncelle.grid(row=3, column=1, padx=20, pady=20)
-
-lbl_hesapsonuc = ttk.Label(anaform, text="")
-lbl_hesapsonuc.grid(row=5, column=1, columnspan=2)
-
-btn_temizle = ttk.Button(anaform, text="Temizle",command=temizle)
-btn_temizle.grid(row=6, column=1)
+input_widget = ttk.Entry(mainView)
+input_widget.grid(row=0, column=2,pady=10)
 
 
+label2_widget = Label(
+      mainView,
+      fg="black",
+      font="Nunito 10 bold"
+    )
+label2_widget.grid(row=1,column=1,padx=10,pady=10)
 
-dolarValue, euroValue = get_exchange_rates()
+cmbx_ex1_widget = ttk.Combobox(mainView)
+cmbx_ex1_widget.grid(row=1,column=2,pady=10,padx=10)
 
-anaform.mainloop()
+label3_widget = Label(
+      mainView,
+      fg="black",
+      font="Nunito 10 bold"
+    )
+label3_widget.grid(row=2,column=1,padx=10,pady=10)
+
+cmbx_ex2_widget = ttk.Combobox(mainView)
+cmbx_ex2_widget.grid(row=2,column=2,padx=10,pady=10)
+
+label4_widget = Label(
+      mainView,
+      fg="red",
+      font="Nunito 10 bold"
+    )
+label4_widget.grid(row=3,column=1,padx=10,pady=10)
+
+labelValue_widget = Label(
+    mainView,
+    fg="red",
+    font="Nunito 10 bold"
+   )
+labelValue_widget.grid(row=3, column=2,padx=10,pady=10)
+
+btn_widget = ttk.Button(
+    mainView,
+    width=25,
+    command=calculateExchange
+   )
+btn_widget.grid(row=4, column=2,padx=10,pady=10)
+
+btnclear_widget = ttk.Button(
+    mainView,
+    width=25,
+    command=clearAll
+   )
+btnclear_widget.grid(row=5,column=2,padx=10,pady=10)
+
+# combobox values list
+cmbx_ex1_widget['values'] = ["USD", "EURO", "TL"]
+cmbx_ex2_widget['values'] = ["USD", "EURO", "TL"]
+
+# texts
+label1_widget['text'] = "Para Girişi: *"
+label2_widget['text'] = "Girişi Yapılan Para: *"
+label3_widget['text'] = "2. Para Tipi: *"
+label4_widget['text'] = "Kur Sonucu: "
+btn_widget['text'] = "Hesapla"
+btnclear_widget['text'] = "Temizle"
+
+
+valueDolar, valueEuro = getExchangeData()
+
+mainView.mainloop()
