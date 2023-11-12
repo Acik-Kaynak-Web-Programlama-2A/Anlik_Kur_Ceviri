5c2af87f813302d755e0c0af74ac600311c97525
LordtheAstro <149760526+LordtheAstro@users.noreply.github.com>
2023-11-12 21:17:46 +0300

    Update A-224526013 Tunahan Bozkurt.py
    kod düzenlemesi

--- A-224526013 Tunahan Bozkurt.py
+++ A-224526013 Tunahan Bozkurt.py
@@ -1,123 +1,100 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 10 09:56:19 2023
+Created on Sun Nov 10 20:15:07 2023
 
-@author: C-117
+@author: Tunahan
 """
+
 
 from tkinter import *
 from tkinter import ttk
 import requests
 
-API_KEY = '3850c4deef802a82010ca7ed'
-BASE_URL = 'https://open.er-api.com/v6/latest/'
+API_ANAHTARI = '3850c4deef802a82010ca7ed'
+ANA_URL = 'https://open.er-api.com/v6/latest/'
 
-def doviz_kurlarini_cek():
+
+
+
+# Open Exchange Rates API'sinden güncel döviz kurlarını çeker
+def doviz_kurlarini_cek(): 
+    # her bir birimin TL karşılığı bu kod bloğu içinde hesaplanıyor. TL karşılığı üzerinden ise birbirleri ile karşılaştırılabiliyor.
     try:
-        # API'den döviz kurlarını al
-        response_try = requests.get(BASE_URL + 'TRY')
-        response_usd = requests.get(BASE_URL + 'USD')
-        response_eur = requests.get(BASE_URL + 'EUR')
-
-        # JSON verilerini çözümle (veriyapısına dönüştür.)
-        data_try = response_try.json()
-        data_usd = response_usd.json()
-        data_eur = response_eur.json()
-
-        # TRY, USD ve EUR kurlarını al
-        roundedtry = 1 / data_try['rates']['TRY']
-        roundeddolar = 1 / data_usd['rates']['TRY']
-        roundedeuro = 1 / data_eur['rates']['TRY']
-
-        return roundedtry, roundeddolar, roundedeuro
-
-    except requests.RequestException as e:
-        print(f"Kur hesaplamalarını hesaplarken hata oluştu: {e}")
+        try_rate = 1 / requests.get(ANA_URL + 'TRY').json()['rates']['TRY']
+        usd_rate = 1 / requests.get(ANA_URL + 'USD').json()['rates']['TRY']
+        eur_rate = 1 / requests.get(ANA_URL + 'EUR').json()['rates']['TRY']
+        return try_rate, usd_rate, eur_rate
+    except requests.RequestException:
         return 0, 0, 0
+
 
 def hesapla(*args):
     try:
-        # Girdi al
-        miktar = float(e_miktar_giris.get())
-        giris_kur = cmb_giriskur.get()
-        cikis_kur = cmb_cikiskur.get()
+        miktar = float(miktar_girisi.get())#float ondalık değer
+        giris_kur, cikis_kur = giris_kur_combobox.get(), cikis_kur_combobox.get()
+        giris_kuru_deger, cikis_kuru_deger = kur_degeri(giris_kur), kur_degeri(cikis_kur)
 
-        # Hesaplamadan önce döviz kurlarını güncelle
-        global roundedtry, roundeddolar, roundedeuro
-        roundedtry, roundeddolar, roundedeuro = doviz_kurlarini_cek()
+        sonuc = miktar * (cikis_kuru_deger / giris_kuru_deger)
+        ciktinin_etiketi.config(text=f"Girilen Miktar: {miktar} {giris_kur}, karşılığı {sonuc:.2f} {cikis_kur}")
 
-        # Giriş ve çıkış kurlarını belirle
-        if giris_kur == "TRY":
-            giris_kur_degeri = roundedtry
-        elif giris_kur == "USD":
-            giris_kur_degeri = roundeddolar
-        elif giris_kur == "EURO":
-            giris_kur_degeri = roundedeuro
-        else:
-            giris_kur_degeri = 0
-
-        if cikis_kur == "TRY":
-            cikis_kur_degeri = roundedtry
-        elif cikis_kur == "USD":
-            cikis_kur_degeri = roundeddolar
-        elif cikis_kur == "EURO":
-            cikis_kur_degeri = roundedeuro
-        else:
-            cikis_kur_degeri = 0
-
-        # Kuru Hesapla ve yazdır
-        sonuc = miktar * (cikis_kur_degeri / giris_kur_degeri)
-        lbl_sonuc.config(text=f"Girilen Miktar: {miktar} {giris_kur}, karşılığı {sonuc:.2f} {cikis_kur}")
     except ValueError:
-        lbl_sonuc.config(text="Geçersiz miktar!")
+        ciktinin_etiketi.config(text="Lütfen geçerli miktar giriniz!")
 
-def otomatik_döviz_kurlarını_güncelle():
-    # Döviz kurlarını otomatik olarak güncelle
-    global roundedtry, roundeddolar, roundedeuro
-    roundedtry, roundeddolar, roundedeuro = doviz_kurlarini_cek()
 
-    # Her 60000 milisaniye (1 dakika) sonra tekrar et
-    anaform.after(60000, otomatik_döviz_kurlarını_güncelle)
+def kur_degeri(kur): 
+    if kur == "TRY":
+        return try_rate
+    elif kur == "USD":
+        return usd_rate
+    elif kur == "EURO":
+        return eur_rate
+    else:
+        return 0
 
-# Ana form oluştur
-anaform = Tk()
-anaform.title("Kur Hesapla")
-anaform.geometry("300x200")
+def otomatik_kurlari_guncelle(): #Değişkenlerin döviz kurlarını ilk kez çekiyor.
+    global try_rate, usd_rate, eur_rate
+    try_rate, usd_rate, eur_rate = doviz_kurlarini_cek()
+    ana_pencere.after(60000, otomatik_kurlari_guncelle)
 
-# Arayüz elemanlarını oluştur
-lbl_miktar = Label(anaform, text="Miktarı Yazınız:")
-e_miktar_giris = ttk.Entry(anaform)
+# Arayüzün genişliği ve yazıların özelliği
+ana_pencere = Tk()
+ana_pencere.title("Güncel Kur Hesap - Tunahan Bozkurt")
+ana_pencere.geometry("340x200")
 
-lbl_giriskur = ttk.Label(anaform, text="Giriş Kur:")
-cmb_giriskur = ttk.Combobox(anaform, values=["TRY", "USD", "EURO"])
+etiket_miktar = Label(ana_pencere, text="Miktar:", font=("Arial", 12), fg="red")
+miktar_girisi = ttk.Entry(ana_pencere, font=("Arial", 12), width=15)
 
-lbl_cikiskur = ttk.Label(anaform, text="Çıkış Kur:")
-cmb_cikiskur = ttk.Combobox(anaform, values=["TRY", "USD", "EURO"])
+etiket_giris_kur = ttk.Label(ana_pencere, text="Bu birimden:", font=("Helvetica", 12), foreground="blue")
+giris_kur_combobox = ttk.Combobox(ana_pencere, values=["TRY", "USD", "EURO"], font=("Helvetica", 12))
 
-lbl_sonuc = ttk.Label(anaform, text="Sonuç:")
+etiket_cikis_kur = ttk.Label(ana_pencere, text="Bu birime dönüştür:", font=("Helvetica", 12), foreground="blue")
+cikis_kur_combobox = ttk.Combobox(ana_pencere, values=["TRY", "USD", "EURO"], font=("Helvetica", 12))
 
-# Elemanları grid'e yerleştir
-lbl_miktar.grid(row=0, column=0, padx=10, pady=10)
-e_miktar_giris.grid(row=0, column=1, padx=10, pady=10)
+ciktinin_etiketi = ttk.Label(ana_pencere, text="Lütfen Para Miktarını Giriniz", font=("Arial", 12))
 
-lbl_giriskur.grid(row=1, column=0, pady=10)
-cmb_giriskur.grid(row=1, column=1, pady=10)
 
-lbl_cikiskur.grid(row=2, column=0, pady=10)
-cmb_cikiskur.grid(row=2, column=1, pady=10)
+# Değerlerin yazacağı bölümlerin pozisyonu ve boyut ayarlamaları
+etiket_miktar.grid(row=0, column=0, padx=10, pady=10)
+miktar_girisi.grid(row=0, column=1, padx=10, pady=10)
 
-lbl_sonuc.grid(row=3, column=0, columnspan=2, pady=10)
+etiket_giris_kur.grid(row=1, column=0, pady=10)
+giris_kur_combobox.grid(row=1, column=1, pady=10)
 
-# Kurları ilk defa güncelle
-roundedtry, roundeddolar, roundedeuro = doviz_kurlarini_cek()
+etiket_cikis_kur.grid(row=2, column=0, pady=10)
+cikis_kur_combobox.grid(row=2, column=1, pady=10)
 
-# Otomatik döviz kuru güncellemeyi başlat
-otomatik_döviz_kurlarını_güncelle()
+ciktinin_etiketi.grid(row=3, column=0, columnspan=2, pady=10)
 
-# Her değer değiştiğinde hesapla fonksiyonunu çağır
-e_miktar_giris.bind("<KeyRelease>", hesapla)
-cmb_giriskur.bind("<<ComboboxSelected>>", hesapla)
-cmb_cikiskur.bind("<<ComboboxSelected>>", hesapla)
+#Program başlangıcındaki döviz kurlarını alır ve değişkenlere atayıp anlık güncellemeyi yapar.
+try_rate, usd_rate, eur_rate = doviz_kurlarini_cek()
+otomatik_kurlari_guncelle()
 
-# Main loop'u başlat
-anaform.mainloop()
+#Hesapla butonu yerine her tuşa basılıp çekildiğinde hesaplama işini gerçekleştirir. 
+miktar_girisi.bind("<KeyRelease>", hesapla)
+giris_kur_combobox.bind("<<ComboboxSelected>>", hesapla)
+cikis_kur_combobox.bind("<<ComboboxSelected>>", hesapla)
+
+
+
+#Tüm kod satırını döngüye alır ve tek hesaplamadan sonra kodun durmasını engeller.
+ana_pencere.mainloop()
